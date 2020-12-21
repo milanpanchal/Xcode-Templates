97a348cbbc24570d01c787001d52ee8078a4d510
Milan Panchal <sam07it22@gmail.com>
2020-12-21 14:41:53 +0530

    Swift View Controller Template

---
+++ File Templates/Personal/JI Swift ViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___:  UIViewController {
+    
+    // MARK:- Life Cycle -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // MARK:- User defined methods -
+    private func setupView() {
+
+    }
+}
